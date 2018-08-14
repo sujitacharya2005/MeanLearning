6e6432e50fe9062944940639b45cdd6050fb53e4
sujitacharay2005 <sujitacharya2005@gmail.com>
2018-08-14 08:12:25 +0530

    26. Calling GET Post - rxjs Subject Subscription instead of event emit

--- meanlearning/src/app/posts/posts.service.ts
+++ meanlearning/src/app/posts/posts.service.ts
@@ -1,14 +1,21 @@
 import { Post } from './post.model';
 import { Injectable } from '../../../node_modules/@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export  class PostsService {
   private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
   getPosts() {
     return [...this.posts];
+  }
+
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
   }
   addPost(title: String, content: String) {
     const post: Post = {title: title, content: content};
     this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
   }
 }
